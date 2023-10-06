a9b5106cba4245f248c0090d21d5cde4caefcd73
Mohammed Aslam M K <117348902+iam-Aslam@users.noreply.github.com>
2023-10-06 12:12:03 +0530

    Sha1 key added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:greatindian/controller/user_provider.dart';
 import 'package:greatindian/view/login_screen/login_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
